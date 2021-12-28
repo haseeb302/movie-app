50333e44584819de7e76fa2f6fc136e70c76c2e2
Abdul Haseeb <mabdulhaseeeb@gmail.com>
2021-12-28 17:04:23 +0500

    base setup @ 4:45

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { NavLink, Outlet } from "react-router-dom";
+import { useState } from 'react';
+
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Movies from './components/Movies';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Grid container direction="column" alignItems="center">
+      <Box mt={10}> 
+        <NavLink to="/trending" className="custom-link" activeClassName="active" exact="true">Trendings</NavLink>  
+        <NavLink to="/popular" className="custom-link" activeClassName="active" exact="true">Popular</NavLink>      
+        <NavLink to="/search" className="custom-link" activeClassName="active" exact="true">Search</NavLink>
+      </Box>
+      <Outlet />
+    </Grid> 
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Trending from './components/Trending';
+import Popular from './components/Popular';
+import Search from './components/Search';
+import MovieDetails from './components/MovieDetails';
+import { Navbar } from "./components/Navbar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="popular" element={<Popular />} />
+          <Route path="trending" element={<Trending />} />
+          <Route path="search" element={<Search />} />  
+        </Route>        
+        <Route path="/movies/:movieId" element={<MovieDetails />} />  
+      </Routes>      
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
