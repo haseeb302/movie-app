6fd339d93ed35a490bbde07f6b72fec59deb2951
Abdul Haseeb <mabdulhaseeeb@gmail.com>
2021-12-28 18:03:36 +0500

    Mrore updates @6

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import Box from '@mui/material/Box';
 import '../styles/movie.css';
 
 const Movies = ({ movie }) => {
